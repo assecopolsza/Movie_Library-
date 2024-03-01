9deb37f18484729adc3dd772f35d4e6b8f993610
Tomasz Rawlik <tomasz.rawlik@asseco.pl>
2024-03-01 14:27:38 +0100

    init

---
+++ MovieLibrary/src/main/java/org/example/models/Actor.java
@@ -0,0 +1,17 @@
+package org.example.models;
+
+public class Actor
+{
+    private String fristname;
+
+    public String getFirstname()
+    {
+        return fristname;
+    }
+
+
+
+    public void setfirstname(String firstname) {
+        this.fristname = firstname;
+    }
+}
